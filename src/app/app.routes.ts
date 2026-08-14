cb94406b04306535db05e1b2f0bb543e9162decf
Wynd-Hoply <wyndhoply@gmail.com>
2026-08-14 11:34:58 -0300

    feat: add routing and components for build-up and home features

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+
+
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./features/home/home/home').then((m) => m.Home),
+  },
+  {
+    path: 'pre-builds',
+    loadComponent: () => import('./features/produtos/lista-produtos/lista-produtos').then((m) => m.ListaProdutos)
+  },
+  {
+    path: 'build-up',
+    loadComponent: () => import('./features/build-up/build-up').then((m) => m.BuildUp)
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
