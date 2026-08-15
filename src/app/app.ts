95a4cb0d600477c95eac8e5db1ea6816937ec23e
João Pedro Lopes Baptista Alves <jblopes2020@gmail.com>
2026-08-15 17:01:47 -0300

    Adição da pasta service para criação do botão de DarkMode

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { Header } from './shared/layout/header/header';
-import { ThemeService } from './services/theme'
+
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, RouterLink, Header],
@@ -9,7 +9,6 @@ import { ThemeService } from './services/theme'
   styleUrl: './app.css'
 })
 export class App {
-  themeService = inject(ThemeService);
 }
 
 
