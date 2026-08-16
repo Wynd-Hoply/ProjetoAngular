22a4022deb50d4d4604542dbe60524ad4d01270e
gabrielgabriel-creator <fernandesgabrielsilva7@gmail.com>
2026-08-16 13:29:24 -0300

    feat: voltei com o RouterLink

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 import { Header } from './shared/layout/header/header';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
