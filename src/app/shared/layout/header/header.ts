39586e654a4694d4af37fef82d01ae7bf7fe12d9
Joao Pedro Lopes Baptista Alves <jblopes2020@gmail.com>
2026-08-24 11:07:54 -0300

    angular

--- src/app/shared/layout/header/header.ts
+++ src/app/shared/layout/header/header.ts
@@ -30,14 +30,14 @@ export class Header {
   readonly mobileMenuOpen = signal(false);
 
   readonly pcComponents = [
-    { label: 'Processadores', icon: 'CPU', route: '/components/cpu', image: 'assets/images/LogoWM.png' },
-    { label: 'Coolers', icon: 'CLK', route: '/components/cooler', image: 'assets/images/LogoWM.png' },
-    { label: 'Placas de vídeo', icon: 'GPU', route: '/components/gpu', image: 'assets/images/LogoWM.png' },
-    { label: 'Placas-mãe', icon: 'MB', route: '/components/motherboard', image: 'assets/images/LogoWM.png' },
-    { label: 'Memórias RAM', icon: 'RAM', route: '/components/ram', image: 'assets/images/LogoWM.png' },
-    { label: 'Armazenamento', icon: 'SSD', route: '/components/storage', image: 'assets/images/LogoWM.png' },
-    { label: 'Gabinetes', icon: 'CASE', route: '/components/case', image: 'assets/images/LogoWM.png' },
-    { label: 'Fontes', icon: 'PSU', route: '/components/psu', image: 'assets/images/LogoWM.png' },
+    { label: 'Processadores', icon: 'CPU', route: '/components/cpu', image: '/assets/images/PROCESSADOR/Processador (1).png' },
+    { label: 'Coolers', icon: 'CLK', route: '/components/cooler', image: '/assets/images/Coolers/cooler (1).png' },
+    { label: 'Placas de vídeo', icon: 'GPU', route: '/components/gpu', image: '/assets/images/PLACA DE VIDEO/PlacaDeVideo (1).png' },
+    { label: 'Placas-mãe', icon: 'MB', route: '/components/motherboard', image: '/assets/images/PLACA MAE/images__1_-removebg-preview.png' },
+    { label: 'Memórias RAM', icon: 'RAM', route: '/components/ram', image: '/assets/images/Ram/images__1_-removebg-preview.png' },
+    { label: 'Armazenamento', icon: 'SSD', route: '/components/storage', image: '/assets/images/ARM/images__7_-removebg-preview.png' },
+    { label: 'Gabinetes', icon: 'CASE', route: '/components/case', image: '/assets/images/Gabinetes/images__1_-removebg-preview.png' },
+    { label: 'Fontes', icon: 'PSU', route: '/components/psu', image: '/assets/images/FONTE/Fonte (1).png' },
   ];
 
   readonly peripherals = [
