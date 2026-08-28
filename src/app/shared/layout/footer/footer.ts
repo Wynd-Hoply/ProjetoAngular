5d434f5af85e448a6e311e2376f30aa68d44fae9
Wynd-Hoply <wyndhoply@gmail.com>
2026-08-28 09:02:09 -0300

    Update footer and header logos; implement theme-based logo switching

--- src/app/shared/layout/footer/footer.ts
+++ src/app/shared/layout/footer/footer.ts
@@ -1,4 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ThemeService } from '../../../core/services/theme';
+
+import { ElementRef, signal } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../../core/services/auth';
+import { Login } from '../../../features/auth/login/login';
+import { Profile } from '../../../features/profile/profile';
 
 @Component({
   selector: 'app-footer',
@@ -6,4 +16,15 @@ import { Component } from '@angular/core';
   templateUrl: './footer.html',
   styleUrl: './footer.css',
 })
-export class Footer {}
+
+export class Footer {
+themeService = inject(ThemeService);
+
+private readonly elementRef = inject(ElementRef<HTMLElement>);
+  private readonly router = inject(Router);
+  authService = inject(AuthService);
+  private readonly dialog = inject(MatDialog);
+
+  readonly piecesMenuOpen = signal(false);
+  readonly mobileMenuOpen = signal(false);
+}
